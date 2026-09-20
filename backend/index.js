35ce8325dea05f788bcb0f73a34156595b2bbb6b
Envyiwnl <parvezmusharf@gmail.com>
2026-09-20 16:57:21 +0530

    Rectified a OTP issue

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,13 @@ const { connect } = require("./db");
 const router = require("./Routes/index");
 const port = 5001;
 
-app.use(cors());
+const corsOptions = {
+  origin: ["https://internshala-beta.vercel.app", "http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
 app.use(bodyparser.json({ limit: "50mb" }));
 app.use(bodyparser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json());
@@ -16,11 +22,7 @@ app.get("/", (req, res) => {
 });
 app.use("/api", router);
 connect();
-app.use((req, res, next) => {
-  req.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
+
 app.listen(port, () => {
   console.log(`Server is running on the port ${port}`);
 });
